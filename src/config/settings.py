c18b20d7d753728fe507efabddb0e843f5f85191
mykytaso <mykyta.soloviov@gmail.com>
2025-03-16 13:43:49 -0400

    Fix environment file path in settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 from pydantic_settings import BaseSettings
@@ -8,8 +7,10 @@ class Settings(BaseSettings):
     BASE_DIR: Path = Path(__file__).parent.parent
     PATH_TO_DB: str = str(BASE_DIR / "database" / "source" / "notes.db")
     DEBUG: bool = False
+    GOOGLE_API_KEY: str = ""
 
     class Config:
-        env_file = ".env"
+        env_file = str(Path(__file__).parent.parent.parent / ".env")
+
 
 settings = Settings()
