ed04d2208291496cc04036c1f170dde35dfdf20b
mykytaso <mykyta.soloviov@gmail.com>
2025-03-16 14:12:34 -0400

    Rename GOOGLE_API_KEY to GENAI_API_KEY in settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     BASE_DIR: Path = Path(__file__).parent.parent
     PATH_TO_DB: str = str(BASE_DIR / "database" / "source" / "notes.db")
     DEBUG: bool = False
-    GOOGLE_API_KEY: str = ""
+    GENAI_API_KEY: str = ""
 
     class Config:
         env_file = str(Path(__file__).parent.parent.parent / ".env")
