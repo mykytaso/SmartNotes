dd6327de54b3b1f84bb2f706236a8895ecd06ed7
mykytaso <mykyta.soloviov@gmail.com>
2025-03-18 13:16:45 -0400

    Add note existence check to analytics endpoints

--- src/routes/analytics.py
+++ src/routes/analytics.py
@@ -10,6 +10,15 @@ from services import get_common_words_phrases, genai_summarize
 router = APIRouter()
 
 
+async def is_note_exists(db: AsyncSession):
+    note_exists = await db.scalar(select(func.count(NoteModel.id) > 0))
+
+    if not note_exists:
+        raise HTTPException(
+            status_code=404, detail="There are no notes in the database."
+        )
+
+
 @router.get("/summary/")
 async def get_note_summary(
     note_id: int = Query(),
@@ -45,12 +54,7 @@ async def get_total_words(db: AsyncSession = Depends(get_db)):
     Returns:
         The total word count of all notes in the database.
     """
-    note_exists = await db.scalar(select(func.count(NoteModel.id) > 0))
-
-    if not note_exists:
-        raise HTTPException(
-            status_code=404, detail="There are no notes in the database."
-        )
+    await is_note_exists(db)
 
     statement = select(
         func.sum(
@@ -68,7 +72,7 @@ async def get_total_words(db: AsyncSession = Depends(get_db)):
 @router.get("/avg-note-length/")
 async def get_avg_note_length(db: AsyncSession = Depends(get_db)):
     """
-    Calculate the average note length across all notes in the database.
+    Calculate the average note length across all notes in the database (by character count).
 
     Args:
         db (AsyncSession): Database session dependency.
@@ -76,6 +80,8 @@ async def get_avg_note_length(db: AsyncSession = Depends(get_db)):
     Returns:
         The average note length rounded to 2 decimal places.
     """
+
+    await is_note_exists(db)
 
     statement = select(
         func.sum(func.length(NoteModel.content)) / cast(func.count(NoteModel.id), Float)
@@ -103,6 +109,8 @@ async def get_most_common_words_or_phrases(
         Common words or phrases (up to 'max_phrase_length' words) that appear at least twice, and their frequencies.
     """
 
+    await is_note_exists(db)
+
     result = await db.execute(select(NoteModel.content))
     notes = result.scalars().all()
 
@@ -114,7 +122,7 @@ async def get_most_common_words_or_phrases(
 @router.get("/top-3-longest-notes/")
 async def get_top_3_longest_notes(db: AsyncSession = Depends(get_db)):
     """
-    Retrieve the top 3 longest notes in the database.
+    Retrieve the top 3 longest notes in the database (by character count).
 
     Args:
         db (AsyncSession): Database session dependency.
@@ -122,6 +130,8 @@ async def get_top_3_longest_notes(db: AsyncSession = Depends(get_db)):
     Returns:
         The top 3 longest notes.
     """
+
+    await is_note_exists(db)
 
     statement = (
         select(NoteModel).order_by(func.length(NoteModel.content).desc()).limit(3)
@@ -145,7 +155,7 @@ async def get_top_3_longest_notes(db: AsyncSession = Depends(get_db)):
 @router.get("/top-3-shortest-notes/")
 async def get_top_3_shortest_notes(db: AsyncSession = Depends(get_db)):
     """
-    Retrieve the top 3 shortest notes in the database.
+    Retrieve the top 3 shortest notes in the database (by character count).
 
     Args:
         db (AsyncSession): Database session dependency.
@@ -153,6 +163,8 @@ async def get_top_3_shortest_notes(db: AsyncSession = Depends(get_db)):
     Returns:
         The top 3 shortest notes.
     """
+
+    await is_note_exists(db)
 
     statement = select(NoteModel).order_by(func.length(NoteModel.content)).limit(3)
     result = await db.execute(statement)
